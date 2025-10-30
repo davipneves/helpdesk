e13b72b2b69c2d236423253cbcd4ba97ee619057
Davi Neves <daviprneves@gmail.com>
2025-10-29 23:39:38 -0300

    update api

--- api.php
+++ api.php
@@ -1,6 +1,6 @@
 <?php
 
-$dbHost = '127.0.0.1';
+$dbHost = '127.0.0.1'; //alterar aqui se der problema
 $dbUser = 'root';
 $dbPass = '';
 $dbName = 'helpdesk_db';
@@ -259,18 +259,14 @@ try {
             $sql = "
                 SELECT T.*, F.nome AS nomeSolicitante, TEC.nome AS nomeTecnico
                 FROM tickets AS T
-<<<<<<< Updated upstream
                 JOIN funcionarios AS F ON T.solicitante_id = F.id
                 LEFT JOIN tecnicos AS TEC ON T.tecnico_id = TEC.id;
-=======
-<<<<<<< Updated upstream
+
                 JOIN funcionarios AS F ON T.codSolicitante = F.codFuncionario
                 LEFT JOIN tecnicos AS TEC ON T.codTecnico = TEC.codTecnico
-=======
+
                 JOIN funcionarios AS F ON T.solicitante_id = F.id
                 LEFT JOIN tecnicos AS TEC ON T.tecnico_id = TEC.id
->>>>>>> Stashed changes
->>>>>>> Stashed changes
             ";
             $whereConditions = [];
             if (!$mostrarTodos) { $whereConditions[] = "T.ativo = 1"; };
